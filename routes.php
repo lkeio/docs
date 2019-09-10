1dfc658e04c6391b36ba7b5e399aa0aaec430777
Larbi Kamal EL Guerch <l.k.elguerch@educasoftware.it>
2019-09-10 16:50:42 +0200

    Defaul home page route

--- routes.php
+++ routes.php
@@ -3,6 +3,9 @@
 use Illuminate\Routing\Router;
 
 /** @var $routing Lkeio\Bases\Routing */
+$routing->router->get('/', function(){
+    return "Home Page";
+});
 
 $routing->router->group(['namespace' => 'Educasoftware\Docs\Controller', 'prefix' => 'docs'], function (Router $router) {
     $router->get('/', ['name' => 'docs.index', 'uses' => 'DocsController@index']);
